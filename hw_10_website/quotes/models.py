8cadcc94021c9c5c76510121c67e016899a761b0
Serhii_Stepanov <stepa.sergey.nov@gmail.com>
2023-08-09 13:29:28 +0300

    save method overridden

--- hw_10_website/quotes/models.py
+++ hw_10_website/quotes/models.py
@@ -19,6 +19,17 @@ class Author(models.Model):
     born_location = models.CharField(max_length=100, null=True)
     photo = models.ImageField(default='default_image.png', upload_to='profile_images', null=True)
 
+    # resizing images
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.photo.path)
+
+        if img.height > 250 or img.width > 250:
+            new_img = (250, 250)
+            img.thumbnail(new_img)
+            img.save(self.photo.path)
+
     def __str__(self):
         return f"{self.fullname}"
 
